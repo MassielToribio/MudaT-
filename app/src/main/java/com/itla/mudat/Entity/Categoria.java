33d3304e2100529e732d6d35f24f7de1f08f8c64
Massiel Toribio <massieltoribio05@gmail.com>
2017-11-29 22:43:17 -0400

    Commit

--- app/src/main/java/com/itla/mudat/Entity/Categoria.java
+++ app/src/main/java/com/itla/mudat/Entity/Categoria.java
@@ -8,4 +8,19 @@ public class Categoria {
     private int id;
     private String descripcion;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- app/src/main/java/com/itla/mudat/dao/CategoriaDbo.java
+++ app/src/main/java/com/itla/mudat/dao/CategoriaDbo.java
@@ -2,9 +2,14 @@ package com.itla.mudat.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.itla.mudat.Entity.Anuncio;
+import com.itla.mudat.Entity.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Usuario on 11/28/2017.
@@ -16,14 +21,39 @@ public class CategoriaDbo {
 
     public CategoriaDbo(Context context) { connection = new DbConnection(context);}
 
-    public void crear( Anuncio anuncio) {
+    public void crear( Categoria categoria) {
 
         SQLiteDatabase db = connection.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
-        cv.put("descripcion", String.valueOf(anuncio.getFecha()));
+        cv.put("descripcion", categoria.getDescripcion());
 
         db.insert("categoria", null, cv);
         db.close();
+    }
+
+    public List<Categoria> buscar(){
+        List<Categoria> categorias = new ArrayList<>();
+
+        SQLiteDatabase db = connection.getWritableDatabase();
+
+        String columnas[] = new String[]{"id", "descripcion" };
+
+        Cursor cursor = db.query("categoria", columnas, null, null, null, null, null );
+
+        cursor.moveToFirst();
+
+        while (!cursor.isAfterLast()){
+            Categoria c = new Categoria();
+            c.setId(cursor.getInt(cursor.getColumnIndex("id")));
+            c.setDescripcion(cursor.getString(cursor.getColumnIndex("descripcion")));
+
+
+            cursor.moveToNext();
+            categorias.add(c);
+        }
+        cursor.close();
+        db.close();
+        return categorias;
     }
 }
